40bad985568d6782c9e582ca8091b3b42636e71a
manju <pass2rahul@gmail.com>
2016-06-08 03:54:30 +0200

    ironrouter setup completed

--- app/client/main.js
+++ app/client/main.js
@@ -1,20 +1,4 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
 import './main.html';
-
-Template.login.onCreated(function loginOnCreated() {
-  console.log("login");
-});
-
-// Template.hello.helpers({
-//   counter() {
-//     return Template.instance().counter.get();
-//   },
-// });
-
-// Template.hello.events({
-//   'click button'(event, instance) {
-//     instance.counter.set(instance.counter.get() + 1);
-//   },
-// });
+import '../imports/ui/templates/login.html'
+import '../imports/client/routes.js'
+import '../imports/client/login.js'

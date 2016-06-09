574b201ead065f4b04ae3d6076441f3ea4a6e2bd
Manjunath Mandya <manjunathms35@gmail.com>
2016-06-10 01:28:47 +0200

    search html and css

--- browser-app/imports/ui/body.js
+++ browser-app/imports/ui/body.js
@@ -1,4 +1,50 @@
 import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze'
  
 import './body.html';
- 
+
+// Template.login.events({
+//     'click .forms': function(){
+// 	background-color : blue;     
+//     }
+// });
+
+
+Router.route('/', function () {
+	name: 'login',
+    this.render('login');
+});
+
+Router.route('/signup',function () {
+	name: 'signup',
+    this.render('signup');	
+});
+
+Router.route('/search',function () {
+	name: 'search',
+    this.render('search');	
+});
+
+
+
+Router.route('/contentAndHeader',function () {
+	name: 'contentAndHeader',
+    this.render('contentAndHeader');	
+});
+
+Router.route('/navigationBar',function () {
+	name: 'navigationBar',
+    this.render('navigation');	
+});
+
+
+
+/*
+Template.login.events({
+	'click button'(event, instance){
+		console.log(Blaze.currentView);
+		Blaze.remove(Template.login);
+		console.log(Template.login)
+	}
+})
+*/
--- browser-app/server/main.js
+++ browser-app/server/main.js
@@ -3,3 +3,20 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+/*
+Router.route('/', {
+	name: 'login',
+    template: 'login'
+});
+
+Router.route('/register', {
+	name: 'register',
+    template: 'register'
+});
+
+/*
+Router.route('/imports/ui/signup.html', function () {
+  this.render('/signup');
+});
+*/

18ef3974714310faf4cb5170e9524925362c474c
manju <pass2rahul@gmail.com>
2016-06-10 02:15:09 +0200

    user collection created and able to login

--- app/imports/client/login.js
+++ app/imports/client/login.js
@@ -1,7 +1,23 @@
 import { Template } from 'meteor/templating';
 
 import '../ui/css/login.css'
+import '../api/collections.js'
 
 Template.login.onCreated(function loginOnCreated() {
   console.log("login");
 });
+
+Template.login.events({
+	'submit .login-form' (event){
+		event.preventDefault();
+		const username = event.target.username.value;
+		const password = event.target.password.value;
+		is_login_valid = validate_login(username, password);
+		if (is_login_valid){
+			console.log("yea happy")
+		}
+		else {
+			alert("invalid login")
+		}
+	}
+})

eae2ed6c87df2be9adf209cb9d7bdc1ad81a7ff6
Manjunath Mandya <manjunathms35@gmail.com>
2016-06-16 18:25:56 +0200

    result page html and css

--- browser-app/imports/ui/body.js
+++ browser-app/imports/ui/body.js
@@ -88,6 +88,23 @@ Template.search.events({
     }
 });
 
+// result page
+Template.result.events({
+    'click .results-list' (event){
+        const is_active = $(event.currentTarget).hasClass('active')
+        if (!is_active){
+            $('.results-list').removeClass('active')
+            $(event.currentTarget).addClass('active')
+        }
+        else {
+            $(event.currentTarget).removeClass('active')
+        }
+    },
+
+    'click #back-search' (event){
+        Router.go("/search");
+    }
+})
 
 
 //for geolocation

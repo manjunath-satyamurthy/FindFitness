d72e5280c8aedc5585005351ec4dbc2466b2b3cd
paritagandhi <paritaorama@gmail.com>
2016-06-21 09:45:19 +0200

    App logo and Splash screen added

---
+++ app/mobile-config.js
@@ -0,0 +1,19 @@
+App.info({
+	author : 'Manjunath Satyamurthy, Parita Gandhi, Manjunath Mandya Surendrakumar, Chaitra Hegde',
+	name : 'FindFitness',
+});
+
+App.icons({
+  	'android_mdpi'   : 'resources/icons/icon.png',
+	'android_hdpi'   : 'resources/icons/icon.png',
+  	'android_xhdpi'  : 'resources/icons/icon.png',
+	'android_xxhdpi' : 'resources/icons/icon.png',
+	'android_xxxhdpi': 'resources/icons/icon.png',
+});
+
+App.launchScreens({
+'android_mdpi_portrait' :'resources/icons/splash_mdpi.png',
+'android_hdpi_portrait' :'resources/icons/splash_hdpi.png',
+'android_xhdpi_portrait' :'resources/icons/splash_xhdpi.png',
+'android_xxhdpi_portrait' :'resources/icons/splash_xxhdpi.png',
+});
